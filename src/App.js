31fdd5da5fbbb758baeb98b6f5aa58becf2c4704
iris-anghel <anghel.iris.bianca@gmail.com>
2018-08-02 22:07:05 +0300

    the map is displayed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ClujMap from './components/ClujMap'
+import Places from './components/Places'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    
+    render() {
+
+        const style = {
+            width: '100vw',
+            height: '80vh'
+        } // move to styles, add a className
+
+        return (
+            <div className="App" style={style}>
+                <header className="App-header">
+                    <h1 className="App-title">Cluj-Napoca Neighborhood Map</h1>
+                </header>
+            
+                <main>
+                    <ClujMap
+
+                    />
+                </main>
+
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/components/ClujMap.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
+// https://www.npmjs.com/package/google-maps-react provides the 'GoogleApiWrapper', a  Higher-Order component for lazy-loading the map
+
+class ClujMap extends Component {
+    render() {
+     
+        const icon = 'http://maps.google.com/mapfiles/ms/icons/coffeehouse.png'
+
+        return (
+            <Map
+                google={this.props.google}
+                initialCenter={ {lat: 46.770331, lng: 23.5852034} }
+                zoom={15}
+            >
+
+            </Map>
+
+        )
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyDt_MPOaWNAG0N14SpXDlbnOc-c_AMcz0w'
+})(ClujMap)
