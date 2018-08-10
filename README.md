de34e2004f4ab223f0b7a80d221b634158e05dc3
iris-anghel <anghel.iris.bianca@gmail.com>
2018-08-10 21:53:26 +0300

    update map details

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
-cluj map ... is a web application that ....
+# Cluj-Napoca Neighborhood Map
+
+Project 8 - [Front-End Web Developer Nano Degree with Udacity & Google]
+(https://eu.udacity.com/course/front-end-web-developer-nanodegree--nd001)
+
+* This is a web application that shows cafes in downtown Cluj-Napoca, Romania.
+* You can see the list by clicking the magnifying glass icon in the top left corner.
+* Each location has a marker assigned to it.
+* An infowindow is triggered by clicking the marker or a location in the sidebar.
+* The infowindow content is provided by Foursquare.
 
 ## Instructions
 
-To use this repository on your local machine:  // development mode
+To use this repository on your local machine:            // development mode
 * clone or download the repository
+* run `nmp install`
 * start the development server with `npm start`
-* on `localhost:3000`
-* a new browser window should automatically open, displaying the app. If it doesn't, navigate to `http://localhost:3000/` in your browser
+* the application will open in your browser at `localhost:3000`
 
-
-To get the site to use service workers please use  // production 
+To get the site to use service workers please use            // production 
 * `npm run build`
 * `serve -s build`
-* and open it on `localhost:5000`.
+* the application will open in your browser at `localhost:5000`
 
 ## Dependencies
 
@@ -21,17 +29,13 @@ The app is built with:
 * [React](https://github.com/facebook/React)
 * [Create React App](https://github.com/facebookincubator/create-react-app)
 * [Node.js](https://github.com/nodejs/node)
-* [npm](https://github.com/npm/cli) or [yarn](https://github.com/yarnpkg/yarn)
+* [npm](https://github.com/npm/cli)
 * [google-maps-react](https://github.com/fullstackreact/google-maps-react)
-* [google maps API]()
+* [Google Maps API](https://cloud.google.com/maps-platform/)
 * [Foursquare API](https://foursquare.com/)
 * [Escape RegExp special characters package](https://github.com/sindresorhus/escape-string-regexp)
 
-<!-- https://snazzymaps.com/style/19955/flat-colors -->
-
-<!-- <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> search -->
-
-<!-- <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>  foursq -->
+This app uses the **flat-colors** theme from [Snazzy Maps](https://snazzymaps.com/style/19955/flat-colors) and the search icon is provided by [Flaticon](https://www.flaticon.com/)
 
 add screenshot
 
--- src/components/ClujMap.js
+++ src/components/ClujMap.js
@@ -1,70 +1,66 @@
 import React, { Component } from 'react'
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
-import flatSnazzy from './flatSnazzy.json'
+import SnazzyMap from './SnazzyMap.json'
 
-// turn into stateless comp if state is not moved here
 class ClujMap extends Component {
+
     render() {
      
         const { filteredLocations, onMarkerClick, selectedPlace, activeMarker, showingInfoWindow } = this.props
         // maybe add bounds
       
-        var defaultIcon = {
-            url: 'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|6B1AB6|40|_|%E2%80%A2', 
-            scaledSize: new this.props.google.maps.Size(25, 40)
-        };
-
         return (
 
+            <main className='map-Container'>  
+                <Map 
+                    google={this.props.google}
+                    initialCenter={{
+                        lat: 46.770181,
+                        lng: 23.591578}}
+                    zoom={15}
+                    styles={MapStyle}
+                    className='map'
+                    role='application'
+                    // bounds={bounds} 
+                >   
 
-            <main className='Map-Container'>  
-                {/* do I need the aside for styling? */}
-            <Map
-                classname='map'
-                role='application'
-                google={this.props.google}
-                initialCenter={{lat: 46.770181, lng: 23.591578}}
-                zoom={15}
-                styles={flatSnazzy}
-                // bounds={bounds} 
-            >
+                    {/* loop over the returned locations and add markers */}
+                    {filteredLocations.map((location, i) => {
+                        return (
+                            <Marker
+                                key={item.id}
+                                title={item.name}
+                                id={item.id}
+                                onClick={onMarkerClick}
+                                name={item.name}
+                                address={item.location.address}
+                                position={{
+                                    lat: item.location.lat,
+                                    lng: item.location.lng }}
+                                icon={{
+                                    path: this.props.google.maps.SymbolPath.CIRCLE,
+                                    scale: 8,
+                                    strokeColor: '#61418D'
+                                }}
+                                animation={(item.name === selectedPlace.title) ? this.props.google.maps.Animation.BOUNCE : null}
+                            />
+                        )
+                    })}
 
-                {/* loop over the returned locations and add markers */}
-                {filteredLocations.map((location, i) => {
-                    return (
-                        <Marker 
-                            icon={defaultIcon}
-                            key={i}
-                            id={location.id}
-                            title={location.name}
-                            name={location.name}
-                            address={location.location.address}
-                            position={{ lat: location.location.lat, lng: location.location.lng }}
-
-                            onClick={onMarkerClick}
-                            // animate only when active
-                            animation={(location.name === selectedPlace.title) ? this.props.google.maps.Animation.DROP : null}
-                            // animation={activeMarker ?
-                            //     (location.name === activeMarker.title ? '1' : '0') : '0'}
-                        />
-                    )
-                })}
-
-                <InfoWindow 
-                        // onOpen={this.windowHasOpened}
-                        // onClose={this.onInfoWindowClose}
+                    <InfoWindow 
                         marker={activeMarker}
                         visible={showingInfoWindow}
-                >
-                    <div>
-                        <h3>{selectedPlace.title}</h3>
-                        <p>Address: {selectedPlace.address}</p>
-                        <p>Info provided by  
-                            <a href="https://foursquare.com" target='_blank' rel="noopener noreferrer"> Foursquare.com</a>
-                        </p>
-                    </div>
-                </InfoWindow>
-            </Map>
+                    >
+                        <div>
+                            <h3>{selectedPlace.name}</h3>
+                            <p>Address: {selectedPlace.address}</p>
+                            <p className="foursquare-attribution">
+                                    Informations by
+                                    <a href="https://foursquare.com" target='_blank' rel="noopener noreferrer"> Foursquare.com</a>
+                            </p>
+                        </div>
+                    </InfoWindow>
+                </Map>
             </main>
         )
     }
